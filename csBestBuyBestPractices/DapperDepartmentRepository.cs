0771385fe0aa625656bb114bd0095b39983196e1
Aliza OConnor <alizaoconnor1@yahoo.com>
2022-07-01 10:17:27 -0500

    completed project

---
+++ csBestBuyBestPractices/DapperDepartmentRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+
+namespace csBestBuyBestPractices
+{
+    public class DapperDepartmentRepository: IDepartmentRepository
+    {
+        private readonly IDbConnection _connection;
+
+        public DapperDepartmentRepository(IDbConnection connection)
+        {
+            _connection = connection;
+        }
+
+        public IEnumerable<Department> GetAllDepartments()
+        {
+            return _connection.Query<Department>("SELECT * FROM Departments;");
+        }
+
+        public void InsertDepartment(string newDepartmentName)
+        {
+            _connection.Execute("INSERT INTO Departments (Name) VALUES (@departmentName);", new { departmentName = newDepartmentName });
+        }
+    }
+}
--- csBestBuyBestPractices/Program.cs
+++ csBestBuyBestPractices/Program.cs
@@ -1,15 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
 
 namespace csBestBuyBestPractices
 {
     internal class Program
     {
+        
         static void Main(string[] args)
         {
+            IConfiguration config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            string connString = config.GetConnectionString("DefaultConnection");
+
+            IDbConnection conn = new MySqlConnection(connString);
+            var repo = new DapperDepartmentRepository(conn);
+
+            Console.WriteLine("Type a new Department name");
+            var newDepartment=Console.ReadLine();
+            repo.InsertDepartment(newDepartment);
+
+            var departments = repo.GetAllDepartments();
+
+            foreach(var dept in departments)
+            {
+                Console.WriteLine(dept.Name);
+            }
+
         }
+
     }
 }
